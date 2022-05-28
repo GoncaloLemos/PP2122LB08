307bc32c1db9416a8b1e13132f02d31324482262
Pedro Miguel Filipe Simões <pedromiguelfilipesimoes@protonmail.com>
2022-05-28 21:24:55 +0100

    UPDATED METHODREFERENCE.JAVA

---
+++ src/F8E1/MethodReference.java
@@ -0,0 +1,7 @@
+package F8E1;
+
+public class MethodReference {
+    public static void main(String args[]) {
+        List<Integer> list = Arrays.asList(2,7,4, 5, 1, 6, 8, 3, 10, 9);
+        for(int number : list) {
+            StaticMethodReference.print(number); } } }
